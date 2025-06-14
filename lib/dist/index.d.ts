dbfafc8c81397e278851881a3608ad5c21811caf
Brett Zamir <brettz9@yahoo.com>
2025-06-14 08:09:19 +0800

    chore: add dist files

---
+++ lib/dist/index.d.ts
@@ -0,0 +1,8 @@
+import { SzType } from "./types";
+export * from "./dezerialize";
+export * from "./zerialize";
+export * from "./types";
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+export type SzPropertyKeysOf<T extends SzType> = KeysOfUnion<Extract<T, {
+    type: "object";
+}>["properties"]>;
